4c6356e122f71ea05a8cff5834969dc1438668f0
Valery <rwn.owo@gmail.com>
2021-12-07 10:46:24 +0300

    fix: all

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,23 @@ export default {
 };
 </script>
 
+<style lang="scss">
+@import "./assets/scss/center.scss";
+
+* {
+  scroll-behavior: smooth;
+}
+section {
+  min-height: calc(100vh - 6rem);
+}
+main {
+  padding-top: 6rem;
+}
+</style>
+
+<style lang="scss" scoped>
+#app,
+.theme-container {
+  height: auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,6 @@
     <main class="main">
       <banner></banner>
       <catalog></catalog>
-      <router-link to="/about">yo</router-link>
-      <router-view></router-view>
     </main>
     <call-button></call-button>
     <!-- <page-footer></page-footer> -->
@@ -34,30 +32,12 @@ export default {
 
 <style>
 @import "../assets/css/variables.css";
-@import "../assets/scss/center.scss";
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap");
-#app,
-.theme-container {
-  height: auto;
-}
+
 body {
   font-family: "Montserrat", sans-serif;
-}
-* {
-  scroll-behavior: smooth;
-}
-section {
-  min-height: calc(100vh - 6rem);
 }
 </style>
 
 <style lang="scss" scoped>
-main {
-  padding-top: 6rem;
-}
-
-@media (min-width: 768px) {
-}
-@media (min-width: 1024px) {
-}
 </style>
