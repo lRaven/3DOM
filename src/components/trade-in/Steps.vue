d400af2190e9af06f2f0f0df86714f51c9b0325f
Valery <rwn.owo@gmail.com>
2021-12-23 17:15:53 +0300

    completed trade-in section

--- src/components/trade-in/Steps.vue
+++ src/components/trade-in/Steps.vue
@@ -36,10 +36,53 @@
 			</div>
 			<div class="steps__img"></div>
 			<div class="steps__stages">
-				<div class="steps__stage"></div>
-				<div class="steps__stage"></div>
-				<div class="steps__stage"></div>
-				<div class="steps__stage"></div>
+				<div class="steps__benefit">
+					<img
+						src="img/icon/trade-in/tick.svg"
+						class="steps__benefit-icon"
+						alt=""
+					/>
+					<h3 class="steps__benefit-title">Кураторство</h3>
+					<p class="steps__benefit-text">
+						Поможем решить все вопросы со сделкой<br />
+						и документами
+					</p>
+				</div>
+				<div class="steps__benefit">
+					<img
+						src="img/icon/trade-in/tick.svg"
+						class="steps__benefit-icon"
+						alt=""
+					/>
+					<h3 class="steps__benefit-title">Быстры сроки</h3>
+					<p class="steps__benefit-text">
+						Проведём сделку<br />
+						в быстрые сроки и учтём все детали
+					</p>
+				</div>
+				<div class="steps__benefit">
+					<img
+						src="img/icon/trade-in/tick.svg"
+						class="steps__benefit-icon"
+						alt=""
+					/>
+					<h3 class="steps__benefit-title">Выберим квартиру</h3>
+					<p class="steps__benefit-text">
+						Предложим квартиры <br />
+						из числа наших проектов
+					</p>
+				</div>
+				<div class="steps__benefit">
+					<img
+						src="img/icon/trade-in/tick.svg"
+						class="steps__benefit-icon"
+						alt=""
+					/>
+					<h3 class="steps__benefit-title">Ипотека</h3>
+					<p class="steps__benefit-text">
+						Если требуется поможем с оформлением ипотеки
+					</p>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -101,12 +144,27 @@
 			gap: 2.4rem;
 			grid-area: 2/1/2/3;
 		}
-		&__stage {
+		&__benefit {
 			box-shadow: 0 0 2rem rgba(0, 0, 0, 0.2);
 			background-color: var(--white);
 			border-radius: 3rem;
+			padding: 3.4rem 3.6rem 4rem 3.6rem;
 			min-height: 27rem;
 			width: 100%;
+			&-icon {
+				margin-bottom: 2.3rem;
+			}
+			&-title {
+				color: var(--blue);
+				font-size: var(--text-25);
+				font-weight: 600;
+				margin-bottom: 1.6rem;
+			}
+			&-text {
+				color: var(--dark);
+				font-size: var(--text-18);
+				line-height: 2.2rem;
+			}
 		}
 	}
 </style>
